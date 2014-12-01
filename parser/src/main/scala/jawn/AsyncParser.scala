3ef6cbf9eed77b7193ea5382e02643c9319f01b1
Erik Osheim <d_m@plastic-idolatry.com>
2014-11-30 20:31:59 -0500

    Make sure async parsing ends at a valid stopping point.
    This commit improves the logic used once finish() has been called
    on an async parser. Thanks to Ross A. Baker for creating the test
    case.
    
    Fixes #16.

--- parser/src/main/scala/jawn/AsyncParser.scala
+++ parser/src/main/scala/jawn/AsyncParser.scala
@@ -228,8 +228,14 @@ final class AsyncParser[J] protected[jawn] (
       Right(results)
     } catch {
       case e: AsyncException =>
-        // we ran out of data, so return what we have so far
-        Right(results)
+        if (done) {
+          // if we are done, make sure we ended at a good stopping point
+          if (state == ASYNC_PREVAL || state == ASYNC_END) Right(results)
+          else Left(ParseException("exhausted input", -1, -1, -1))
+        } else {
+          // we ran out of data, so return what we have so far
+          Right(results)
+        }
 
       case e: ParseException =>
         // we hit a parser error, so return that error and results so far
