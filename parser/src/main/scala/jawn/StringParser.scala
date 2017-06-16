5aac8c24226c5d3945fd1aea4e949177b5a912f0
Travis Brown <travisrobertbrown@gmail.com>
2017-06-16 08:14:28 -0500

    Use CharBuffer and new AsChars

--- parser/src/main/scala/jawn/StringParser.scala
+++ parser/src/main/scala/jawn/StringParser.scala
@@ -1,5 +1,7 @@
 package jawn
 
+import java.nio.CharBuffer
+
 /**
  * Basic in-memory string parsing.
  *
@@ -19,48 +21,7 @@ private[jawn] final class StringParser[J](s: String) extends SyncParser[J] with 
   final def reset(i: Int): Int = i
   final def checkpoint(state: Int, i: Int, stack: List[FContext[J]]) {}
   final def at(i: Int): Char = s.charAt(i)
-  final def at(i: Int, j: Int): CharSequence = new Slice(s, i, j)
+  final def at(i: Int, j: Int): CharSequence = CharBuffer.wrap(s, i, j)
   final def atEof(i: Int) = i == s.length
   final def close() = ()
-}
-
-import java.lang.Math
-
-final class Slice(s: String, start: Int, limit: Int) extends CharSequence {
-  def charAt(k: Int): Char =
-    s.charAt(start + k)
-  def length: Int =
-    limit - start
-  def subSequence(i: Int, j: Int): CharSequence =
-    new Slice(s, start + i, Math.min(start + j, limit))
-  override def equals(that: Any): Boolean =
-    that match {
-      case cs: CharSequence =>
-        if (this eq cs) return true
-        val len = length
-        if (len != cs.length) return false
-        var i = 0
-        while (i < len) {
-          if (charAt(i) != cs.charAt(i)) return false
-          i += 1
-        }
-        true
-      case _ =>
-        false
-    }
-
-  // uses a starting prime number and a prime multiplier to get good
-  // hashing behavior in most cases.
-  override def hashCode: Int = {
-    var code: Int = 3267000013 // prime
-    var i: Int = 0
-    while (i < len) {
-      code = code * 3628273133 + charAt(i)
-      i += 1
-    }
-    code
-  }
-
-  override def toString: String =
-    s.substring(start, limit)
 }
